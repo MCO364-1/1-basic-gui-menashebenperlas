b35700d7e3265a3822722d96ee40f11e08b511c0
menashebenperlas <mbenperl@student.touro.edu>
2025-02-02 17:02:50 -0500

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame window = new JFrame();
+        window.setTitle("Calculator");
+        window.setSize(800, 600);
+        window.setLayout(new BorderLayout());
+
+        JTextField text1 = new JTextField();
+        JTextField text2 = new JTextField();
+        window.add(text1, BorderLayout.NORTH);
+        window.add(text2, BorderLayout.SOUTH);
+
+        JButton pressMeButton = new JButton("Click To Add");
+        window.add(pressMeButton, BorderLayout.CENTER);
+
+        JLabel label = new JLabel("     ");
+        window.add(label , BorderLayout.WEST);
+
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        pressMeButton.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String one  = text1.getText();
+                String two  = text2.getText();
+                int sum  = Integer.parseInt(one) + Integer.parseInt(two);
+
+                label.setText("Sum: " + sum);
+            }
+        });
     }
 }
